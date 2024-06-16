b7ac1bece700b74d221707f8e88e7473028130b2
Warren <umbach4@gmail.com>
2024-06-16 09:57:04 -0400

    added instructions for autopair script

--- DataReadTest.py
+++ DataReadTest.py
@@ -9,6 +9,32 @@
 #install adafruit-circuitpython-adxl34x
 #pip3 install adafruit-circuitpython-adxl34x
 
+#use bluetoothctl and set system-alias to skate
+#power on
+#agent on
+#advertise on
+#pairable on
+#pair <phone MAC address>
+#complete pass key check
+#trust <phone MAC address>
+#devices to check connection
+
+#open termius and setup ssh profile with skate.local and the user and pass for the device
+#cd to skate and activate .env
+#python test.py
+
+#make a scripts dir - mkdir -p ~/scripts
+#make an autopair script - nano ~/scripts/autopair
+#add:
+# #!/bin/bash
+# bluetoothctl << EOF
+# connect [enter your MAC add]
+# EOF
+#save the file and make it executable with:
+# chmod +X ~/scripts/autopair
+
+#reboot to test
+
 import time
 import board
 import adafruit_adxl34x
