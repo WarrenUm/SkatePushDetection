360f972863beadb6183bf983ecf33da0e43ec343
Warren <umbach4@gmail.com>
2024-06-19 17:21:00 -0400

    fix font

--- LCDTesting.py
+++ LCDTesting.py
@@ -66,7 +66,7 @@ x = 0
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
+font = ImageFont.truetype("DejaVuSans.ttf", 24)
 
 # Turn on the backlight
 backlight = digitalio.DigitalInOut(board.D22)
