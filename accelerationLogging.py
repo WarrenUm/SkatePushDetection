1690cb9d4dbdbd585c419b56762ac5508f68595b
Warren <umbach4@gmail.com>
2024-06-21 11:53:45 -0400

    disable gps

--- accelerationLogging.py
+++ accelerationLogging.py
@@ -10,10 +10,10 @@ from PIL import Image, ImageDraw, ImageFont
 from adafruit_rgb_display import st7789
 
 
-i2cgps = board.I2C()  # For using the built-in STEMMA QT connector on a microcontroller
-gps = adafruit_gps.GPS_GtopI2C(i2cgps)
-gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-gps.send_command(b"PMTK220,1000")
+# i2cgps = board.I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+# gps = adafruit_gps.GPS_GtopI2C(i2cgps)
+# gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+# gps.send_command(b"PMTK220,1000")
 
 i2c = board.I2C()
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
@@ -130,19 +130,19 @@ while True:
         print("buttonA Pressed")
         newLogfile()
         
-    if gps.has_fix:
-        print(
-                "Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
-                    gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
-                    gps.timestamp_utc.tm_mday,  # struct_time object that holds
-                    gps.timestamp_utc.tm_year,  # the fix time.  Note you might
-                    gps.timestamp_utc.tm_hour,  # not get all data like year, day,
-                    gps.timestamp_utc.tm_min,  # month!
-                    gps.timestamp_utc.tm_sec,
-                )
-            )
-    else:
-        print("no gps fix")
+    # if gps.has_fix:
+    #     print(
+    #             "Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
+    #                 gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
+    #                 gps.timestamp_utc.tm_mday,  # struct_time object that holds
+    #                 gps.timestamp_utc.tm_year,  # the fix time.  Note you might
+    #                 gps.timestamp_utc.tm_hour,  # not get all data like year, day,
+    #                 gps.timestamp_utc.tm_min,  # month!
+    #                 gps.timestamp_utc.tm_sec,
+    #             )
+    #         )
+    # else:
+    #     print("no gps fix")
 
 
     timestamp = time.monotonic()
