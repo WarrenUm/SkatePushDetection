9b3b72a2cadaa3ec82148c6632645e8923602ccc
Warren <umbach4@gmail.com>
2024-06-21 11:48:13 -0400

    check for gps fix

--- accelerationLogging.py
+++ accelerationLogging.py
@@ -130,16 +130,19 @@ while True:
         print("buttonA Pressed")
         newLogfile()
         
-    print(
-            "Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
-                gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
-                gps.timestamp_utc.tm_mday,  # struct_time object that holds
-                gps.timestamp_utc.tm_year,  # the fix time.  Note you might
-                gps.timestamp_utc.tm_hour,  # not get all data like year, day,
-                gps.timestamp_utc.tm_min,  # month!
-                gps.timestamp_utc.tm_sec,
+    if gps.has_fix:
+        print(
+                "Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
+                    gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
+                    gps.timestamp_utc.tm_mday,  # struct_time object that holds
+                    gps.timestamp_utc.tm_year,  # the fix time.  Note you might
+                    gps.timestamp_utc.tm_hour,  # not get all data like year, day,
+                    gps.timestamp_utc.tm_min,  # month!
+                    gps.timestamp_utc.tm_sec,
+                )
             )
-        )
+    else:
+        print("no gps fix")
 
 
     timestamp = time.monotonic()
