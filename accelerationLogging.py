323d415d2738c925234b37df8d8e73e953dfc71c
Warren <umbach4@gmail.com>
2024-06-27 09:24:23 -0400

    separate gps test file

--- accelerationLogging.py
+++ accelerationLogging.py
@@ -10,11 +10,6 @@ from PIL import Image, ImageDraw, ImageFont
 from adafruit_rgb_display import st7789
 
 
-# i2cgps = board.I2C()  # For using the built-in STEMMA QT connector on a microcontroller
-# gps = adafruit_gps.GPS_GtopI2C(i2cgps)
-# gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-# gps.send_command(b"PMTK220,1000")
-
 i2c = board.I2C()
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
 accelerometer.range = Range.RANGE_16_G
@@ -129,21 +124,6 @@ while True:
     if buttonToggleBacklight and not buttonStartNewLog.value:  # just button A pressed
         print("buttonA Pressed")
         newLogfile()
-        
-    # if gps.has_fix:
-    #     print(
-    #             "Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
-    #                 gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
-    #                 gps.timestamp_utc.tm_mday,  # struct_time object that holds
-    #                 gps.timestamp_utc.tm_year,  # the fix time.  Note you might
-    #                 gps.timestamp_utc.tm_hour,  # not get all data like year, day,
-    #                 gps.timestamp_utc.tm_min,  # month!
-    #                 gps.timestamp_utc.tm_sec,
-    #             )
-    #         )
-    # else:
-    #     print("no gps fix")
-
 
     timestamp = time.monotonic()
     accelerationX = accelerometer.acceleration[0]
---
+++ gpsTest.py
@@ -0,0 +1,25 @@
+import time
+import os
+import board
+import adafruit_gps
+
+
+i2cgps = board.I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+gps = adafruit_gps.GPS_GtopI2C(i2cgps)
+gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+gps.send_command(b"PMTK220,1000")
+
+while True:
+    if gps.has_fix:
+            print(
+                    "Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
+                        gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
+                        gps.timestamp_utc.tm_mday,  # struct_time object that holds
+                        gps.timestamp_utc.tm_year,  # the fix time.  Note you might
+                        gps.timestamp_utc.tm_hour,  # not get all data like year, day,
+                        gps.timestamp_utc.tm_min,  # month!
+                        gps.timestamp_utc.tm_sec,
+                    )
+                )
+    else:
+        print("no gps fix")
