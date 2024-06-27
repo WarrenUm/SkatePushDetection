3d183ddaff9db4a9f222eb318f0158e3d761af4d
Warren <umbach4@gmail.com>
2024-06-27 09:38:28 -0400

    stemma

--- gpsTest.py
+++ gpsTest.py
@@ -4,7 +4,7 @@ import board
 import adafruit_gps
 
 
-i2cgps = board.I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+i2cgps = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 gps = adafruit_gps.GPS_GtopI2C(i2cgps)
 gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
 gps.send_command(b"PMTK220,1000")
